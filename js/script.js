f1274f9d3974673e929a6bfa7dcc3b68f35b95c5
Margaret Kato <minim4455@gmail.com>
2026-02-28 12:27:48 -0500

    cleaned files, data uploaded and parsed

--- js/script.js
+++ js/script.js
@@ -2,138 +2,33 @@ import 'https://esm.sh/d3-transition';
 
 console.log('D3 Version:', d3.version);
 
-let allData = []
-let xScale, yScale, sizeScale
-
-let xVar = 'income', yVar = 'lifeExp', sizeVar = 'population', targetYear = 2000
+//function to parse date
+var parseDate = d3.timeParse("%Y%m%d");
 
 //load data after page is loaded
 function init(){
-    d3.csv("./data/gapminder_subset.csv", function(d){
+    d3.csv("./data/weather.csv", function(d){
         return {  
         // Besides converting the types, we also simpilify the variable names here. 
-        country: d.country,
-        continent: d.continent,
-        year: +d.year, // using + to convert to numbers; same below
-        lifeExp: +d.life_expectancy, 
-        income: +d.income_per_person, 
-        gdp: +d.gdp_per_capita, 
-        childDeaths: +d.number_of_child_deaths,
-        population: +d.population,
-        // Your turn: 
-        // convert d.population, and assign it to population
+        station: d.station,
+        state: d.state,
+        latitude: +d.latitude,
+        longitude: +d.longitude, 
+        elevation: +d.elevation, 
+        date: parseDate(d.date), 
+        minTempF: +d.TMIN,
+        maxTempF: +d.TMAX,
+        avgTempF: +d.TAVG,
+        avgWind: +d.AWND,
+        snowfallIn: +d.SNOW,
+        snowDepthIn: +d.SNWD,
+        precipIn: +d.PRCP
      }
     })
     .then(data => {
-           // console.log(data)
-            allData = data
-            // placeholder for building vis
-            // placeholder for adding listerners
-
-            setupSelector()
-            
-            // Initial rendering steps:
-            // P.S. You could move these into setupSelector(), 
-            // but calling them separately makes the flow clearer.
-            updateAxes()
-            updateVis()
-            addLegend()
-        })
+        console.log(data)
+    })
     .catch(error => console.error('Error loading data:', error));
 }
 
-function setupSelector(){
-  // Handles UI changes (sliders, dropdowns)
-  // Anytime the user tweaks something, this function reacts.
-  // May need to call updateAxes() and updateVis() here when needed!
-
-  var slider = d3
-    .sliderHorizontal()
-    .min(0)
-    .max(10)
-    .step(1)
-    .width(300)
-    .displayValue(false)
-    .on('onchange', (val) => {
-      d3.select('#value').text(val);
-    });
-
-  d3.select('#slider')
-    .append('svg')
-    .attr('width', 500)
-    .attr('height', 100)
-    .append('g')
-    .attr('transform', 'translate(30,30)')
-    .call(slider);
-}
-
-function updateAxes(){
-  // Draws the x-axis and y-axis
-  // Adds ticks, labels, and makes sure everything lines up nicely
-}
-
-function updateVis(){
-  // Draws (or updates) the bubbles
-}
-
-function addLegend(){
- // Adds a legend so users can decode colors
-}
-
 window.addEventListener('load', init);
-
-//make margins
-const margin = {top: 40, right: 40, bottom: 40, left: 60};
-const width = 800 - margin.left - margin.right;
-const height = 600 - margin.top - margin.bottom;
-
-// Create SVG
-const svg = d3.select('#vis')
-    .append('svg')
-    .attr('width', width + margin.left + margin.right)
-    .attr('height', height + margin.top + margin.bottom)
-    .append('g')
-    .attr('transform', `translate(${margin.left},${margin.top})`);
-
-  // Create x scale
-xScale = d3.scaleLinear()
-    .domain([0, d3.max(allData, d => d[xVar])])
-    .range([0, width]);
-const xAxis = d3.axisBottom(xScale)
-
-svg.append("g")
-    .attr("class", "axis")
-    .attr("transform", `translate(0,${height})`) // Position at the bottom
-    .call(xAxis);
-
-    //create y scale
-yScale = d3.scaleLinear()
-    .domain([0, d3.max(allData, d => d[yVar])])
-    .range([height, 0]);
-const yAxis = d3.axisBottom(yScale)
-
-svg.append("g")
-    .attr("class", "axis")
-    .attr("transform", `translate(0,${height})`) // Position at the bottom
-    .call(yAxis);
-
-sizeScale = d3.scaleSqrt()
-    .domain([0, d3.max(allData, d => d[sizeVar])]) // Largest bubble = largest data point 
-    .range([5, 20]);
-
-// X-axis label
-svg.append("text")
-    .attr("x", width / 2)
-    .attr("y", height + margin.bottom - 20)
-    .attr("text-anchor", "middle")
-    .text(xVar) // Displays the current x-axis variable
-    .attr('class', 'labels')
-
-// Y-axis label (rotated)
-svg.append("text")
-    .attr("transform", "rotate(-90)")
-    .attr("x", -height / 2)
-    .attr("y", -margin.left + 40)
-    .attr("text-anchor", "middle")
-    .text(yVar) // Displays the current y-axis variable
-    .attr('class', 'labels')
